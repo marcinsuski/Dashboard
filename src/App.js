196ee85ae31c6c281d8928d55d7f735d6d96731e
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2023-01-12 19:10:47 +0100

    added calendar scene

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import Team from './scenes/team';
 import Contacts from './scenes/contacts';
 import Invoices from './scenes/invoices';
 import Form from './scenes/form';
+import Calendar from './scenes/calendar';
 // import Bar from './scenes/bar';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
-// import Calendar from './scenes/calendar';
 // import FAQ from './scenes/faq';
 // import Geography from './scenes/geography';
 
@@ -36,7 +36,7 @@ function App() {
                         {/* <Route path="/line" element={<Line/>} /> */}
                         {/* <Route path="/faq" element={<FAQ/>} /> */}
                         {/* <Route path="/geography" element={<Geography/>} /> */}
-                        {/* <Route path="/calendar" element={<Calendar/>} /> */}
+                        <Route path="/calendar" element={<Calendar/>} />
                       </Routes>
                     </main>
                 </div>
